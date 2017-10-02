9f146114490da83131e493b78a54096650598f41
RiskyClick <jonn.thinkrtc@gmail.com>
2017-10-02 18:15:27 +0000

    add dummy data as placeholder

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog| 
+    Blog.create!(
+        title: "Ruby on Rails Installation #{blog}",
+        body: "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested"
+        )
+end
+
+puts "10 blogs created"
+
+5.times do |skill| 
+    Skill.create!(
+        title: "Ruby on Rails",
+        percent_utilized: 70
+        )
+end
+
+puts "5 skills"
+
+9.times do |portofolio|
+    Portofolio.create!(
+        title: "Ruby on Rails",
+        subtitle: "sudo service postgresql start",
+        body: "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested",
+        main_image: "http://via.placeholder.com/600x400",
+        thumb_image: "http://via.placeholder.com/350x200"
+        )
+end
+
+puts "9 portofolios"
