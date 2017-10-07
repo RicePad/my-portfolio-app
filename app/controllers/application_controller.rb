b0848dbeefd1542ebc8ab79c509423afa3a2cfd1
RiskyClick <jonn.thinkrtc@gmail.com>
2017-10-07 20:47:00 +0000

    add session tracker on application controller and application.html.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   #add name parameters to devise authentication:
- include DeviseWhitelist
+  include DeviseWhitelist
+  
+  before_action :set_source
+  
+  def set_source
+    session[:source] = params[:q] if params[:q]
+  end
   
 
 
