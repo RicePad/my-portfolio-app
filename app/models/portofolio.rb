52bc52a4dd4dc93662b82214f98cf3b0d47705c7
RiskyClick <jonn.thinkrtc@gmail.com>
2017-10-15 17:29:47 +0000

    add style to portofolio uploader

--- app/models/portofolio.rb
+++ app/models/portofolio.rb
@@ -12,12 +12,12 @@ class Portofolio < ApplicationRecord
     where(subtitle: "Angular")
   end
   
-  after_initialize :set_defaults
+  # after_initialize :set_defaults
   
-  def set_defaults
-    self.main_image ||= "http://via.placeholder.com/600x400"
-    self.thumb_image ||= "http://via.placeholder.com/350x200"
-  end
+  # def set_defaults
+  #   self.main_image ||= "http://via.placeholder.com/600x400"
+  #   self.thumb_image ||= "http://via.placeholder.com/350x200"
+  # end
     
   def self.by_position 
     order("position ASC")
