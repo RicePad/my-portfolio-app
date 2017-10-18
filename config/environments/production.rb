1e741263551d3e7e2bf1c15aa8f99f47866891ea
RiskyClick <jonn.thinkrtc@gmail.com>
2017-10-18 01:43:05 +0000

    update genm file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+    config.action_cable.allowed_request_origins = ['https://my-portofolio-today.herokuapp.com', 'http://devcamp-portfolio.herokuapp.com']
+  config.action_cable.url = "wss://my-portofolio-today.herokuapp.com/cable"
 end
