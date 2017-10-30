23ae16d6f3275be6fe8db8781e19e1ac5a0e12fb
RiskyClick <jonn.thinkrtc@gmail.com>
2017-10-30 04:47:49 +0000

    update application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-
 //= require rails-ujs
-//= require bootstrap-sprockets
 //= require jquery3
 //= require jquery-ui
+//= require bootstrap-sprockets
 //= require html.sortable
 //= require popper
 //= require bootstrap
